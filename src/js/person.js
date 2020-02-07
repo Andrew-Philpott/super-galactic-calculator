8d8600ec18182c1383dd74352da6efa6eb6b7248
Andrew Philpott <andrewphilpott92@gmail.com>
2020-02-07 11:56:01 -0800

    Add lifeExpectancy property and set it to 73.4 if the gender is Male.

--- src/js/person.js
+++ src/js/person.js
@@ -2,6 +2,10 @@ export class Person {
   constructor(age, gender) {
     this.age = age;
     this.gender = gender
+    this.lifeExpectancy;
+    if(this.gender === 'Male') {
+      this.lifeExpectancy = 73.4;
+    } 
   }
   
   getAge() {
